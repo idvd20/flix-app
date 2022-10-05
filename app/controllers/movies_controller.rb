0632e9b61c22d51e6241e886b94fa771cf1320f0
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-05 11:13:35 +0800

    Flash practice

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,8 @@ class MoviesController < ApplicationController
 
     def update
         @movie = Movie.find(params[:id])        
-        
-        if @movie.update(movie_params)
-            redirect_to @movie            
+        if @movie.update(movie_params)            
+            redirect_to @movie, notice: "Movie successfully updated!"    
         else
             render :edit, status: :unprocessable_entity
         end
@@ -29,7 +28,7 @@ class MoviesController < ApplicationController
     def create        
         @movie = Movie.new(movie_params)
         if @movie.save
-            redirect_to @movie            
+            redirect_to @movie, notice: "Movie sucessfully created."         
         else
             render :new, status: :unprocessable_entity
         end
@@ -38,7 +37,7 @@ class MoviesController < ApplicationController
     def destroy
         @movie = Movie.find(params[:id])
         @movie.destroy
-        redirect_to movies_url, status: :see_other
+        redirect_to movies_url, status: :see_other, alert: "Movie deleted!"
     end
 
     private
