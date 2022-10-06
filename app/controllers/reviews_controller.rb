3b214d17ebbf98171e41b94d4905d9c66e04153d
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-06 17:20:50 +0800

    One-to-Many: Forms practice

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,36 @@
 class ReviewsController < ApplicationController
 
-    def index
-        @movie = Movie.find(params[:movie_id])
+    before_action :write_review
+
+    def index  
         @reviews = @movie.reviews
     end
+
+    def new
+        @review = @movie.reviews.new
+    end
+
+    def create
+        @review = @movie.reviews.new(movie_params)
+
+        if @review.save
+            redirect_to @movie, notice: "Thanks for your review!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        fail
+    end
+
+    private
+
+        def movie_params
+            params.require(:review).permit(:name, :comment, :stars)
+        end
+
+        def write_review
+            @movie = Movie.find(params[:movie_id])
+        end
 end
