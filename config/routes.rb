abdf3d1e114bf9e43ae67aefdbbc6c8384d7c751
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-11 17:19:56 +0800

    Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   
   resources :users
 
+  resource :session, only: [:new, :create, :destroy]
+
   get "signup" => "users#new"
+  get "signin" => "sessions#new"
 end
