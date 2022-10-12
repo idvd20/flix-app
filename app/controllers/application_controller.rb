9cedcdef0c315d7e865000fb1029c51bcd204d1d
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-12 17:22:41 +0800

    Admin Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
             end
         end
 
+        def require_admin
+            redirect_to root_url, alert: "Unauthorized access!" unless current_user_admin?
+        end
+
         def current_user
             @current_user ||= User.find(session[:user_id]) if session[:user_id]
         end
@@ -17,6 +21,11 @@ class ApplicationController < ActionController::Base
             current_user == user
         end
 
+        def current_user_admin?
+            current_user && current_user.admin?
+        end
+
         helper_method :current_user
         helper_method :current_user?
+        helper_method :current_user_admin?
 end
